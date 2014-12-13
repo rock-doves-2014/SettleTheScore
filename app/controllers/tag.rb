a589d62cae3250d4feb3571617083a6b73d39955
Jorge Rubio C. & Luke Kedziora <pair+jorgerc85.lukekedz@devbootcamp.com>
2014-12-13 13:47:27 -0500

    Complete tag search feature

--- app/controllers/tag.rb
+++ app/controllers/tag.rb
@@ -3,3 +3,9 @@ get '/tag/:id' do |id|
   @confrontations = @tag.confrontations
   erb :"tag/tag"
 end
+
+get '/tag' do
+  @tag = Tag.find_by(name: params[:tag])
+  @confrontations = @tag.confrontations
+  erb :"tag/tag"
+end
